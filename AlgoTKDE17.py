e7fdf40af111578729d1943b2d4c96022dd26ebb
yllkav88 <99736269+yllkav88@users.noreply.github.com>
2022-10-05 09:47:41 +0200

    Update AlgoTKDE17.py
    Fixed TKDE problems

--- AlgoTKDE17.py
+++ AlgoTKDE17.py
@@ -5,7 +5,6 @@ import math
 from src.query import Query
 from src.graph import UGraph
 
-T = 32 # Number of Monte-Carlo sample of possible worlds
 def compute_reach(s, t, d):
     probGraph = UGraph()
     for e in G.edges:
@@ -34,65 +33,59 @@ def compute_Pstar(e): #approximation of P*(e) using the upper bound
     u=e[0]
     v=e[1]
     prob=G[u][v]['prob']
-    num=compute_ub((u,v), s, t, d)
+    num=e[3]
     return num/prob
 
 #compute UB on the numerator of P(e)
-def compute_ub(e, s, t, d):
-    ub=0.0
-    path_l=list(nx.all_simple_paths(G, s, t, cutoff=d))
-    paths=path_l.copy()
+def compute_ub(e, path_l, Rstd_G):
     Je=[]
-    save_prob={} 
-    i=0
     sum_pr=0
     for path in map(nx.utils.pairwise, path_l):
         p=(list(path))
-        pr_j=compute_prob_path(p)
-        save_prob[i]=pr_j
-        i+=1
-        if e in p:
+        if ((e[0], e[1]) in p) or ((e[1], e[0]) in p):
             Je.append(p)
             sum_pr+=compute_prob_path(p)
-    pr_j=0
-    Rstd_G=compute_reach(s,t,d)
-    for p in Je:
-        pr_j=compute_prob_path(list(p))
-        ub+=pr_j*(1-(Rstd_G-sum_pr))
+    #pr_j=0
+    ub=sum_pr*(1-(Rstd_G-sum_pr))
 
     return ub
 
-#tuple((e[0], e[1]))
+
 
 def find_e(G, s, t, d, e_clean ): #Algorithm 1 of the paper
     NL=[]
-    DL=[]  
+    DL=[] 
+    path_l=list(nx.all_simple_paths(G, s, t, cutoff=d)) 
+    Rstd_G=compute_reach(s,t,d)
     for i in range(len(e_clean)):
-        U_e=compute_ub(e_clean[i], s, t, d)
+        U_e=compute_ub(e_clean[i], path_l, Rstd_G)
         NL.append((e_clean[i][0],e_clean[i][1], 1.0, U_e))
     
-    #NL=sorted(NL.items(), key=lambda item: item[1], reverse=True)
-    NL.sort(key=lambda a: a[3])    
+    NL.sort(key=lambda a: a[3], reverse=True)    
+    #print(NL)
     e_clean.sort(key=lambda a: a[3])
     DL=e_clean.copy()
+    #print(DL)
     Pmax=-math.inf
     e=(-1,-1, -1.0, -1.0)
     f=True
+
     while f==True:
         en=NL.pop(0)
-        print('en: ',en)
         ed=DL.pop(0)
         Pstar_en=compute_Pstar(en)
         if Pstar_en>Pmax:
             e=en
-            Pmax=Pstar_en
-            
+            Pmax=Pstar_en  
+        for en1 in NL:
+            if (en1[0]==ed[0] and en1[1]==ed[1]) or (en1[0]==ed[1] and en1[1]==ed[0]):
+                ed=en1         
         Pstar_ed=compute_Pstar(ed)
         if Pstar_ed>Pmax:
             e=ed
             Pmax=Pstar_ed
             
-        if en[1]/ed[3]<Pmax: 
+        if en[3]/ed[3]<Pmax: 
             f=False
         
     return e
@@ -102,32 +95,38 @@ def find_e(G, s, t, d, e_clean ): #Algorithm 1 of the paper
 
 
  
-G=nx.read_edgelist("RelComp/test_wgraph.txt", delimiter=" ", create_using=nx.Graph(), nodetype=int, data=(("weight", float), ("prob", float)))
-print(G.edges)
-# e_clean=[(3, 0, 1.0, 0.5),
-#          (1, 2, 1.0, 0.6),
-#          (1, 3, 1.0, 0.3)] #Edges to clean
-# e_clean = [
-#     (0, 4, 1.0, 1.0),
-#     (1, 2, 1.0, 0.25),
-#     (2, 0, 1.0, 0.75),
-#     (2, 6, 1.0, 0.75),
-#     (3, 4, 1.0, 0.5),
-#     (5, 1, 1.0, 0.5),
-#     (6, 5, 1.0, 0.5),
-#     (6, 1, 1.0, 0.75),
-#     (6, 4, 1.0, 0.75)
-# ]
-#e_clean=G.edges(data=True)
-e_clean = [(e[0],e[1],e[2]['weight'],e[2]['prob']) for e in G.edges(data=True)]
-print(e_clean)
+G=nx.read_edgelist("test_graph_TKDE.txt", delimiter=" ", create_using=nx.Graph(), nodetype=int, data=(("weight", float), ("prob", float)))
+#print(G.edges)
+
+
+e_clean=[(3, 0, 1.0, 0.5),
+          (0, 2, 1.0, 0.7),
+          (1, 3, 1.0, 0.3)] #Edges to clean
+
+
+#e_clean = [(e[0],e[1],e[2]['weight'],e[2]['prob']) for e in G.edges(data=True)]
+print("Candidate edges: ", e_clean)
 s=3
 t=2
 d=3
+budget=2
 
 
+
+#pruning strategy
+#pruning by reverse shortest path
+for v in G.nodes():
+    ds_v=nx.dijkstra_path_length(G, v, s, weight='weight')
+    dt_v=nx.dijkstra_path_length(G, v, t, weight='weight')
+    e_copy=e_clean.copy()
+    if ds_v+dt_v>d:
+        for a, b, w, p in e_copy:
+            if a==v or b==v:
+                e_clean.remove((a,b,w,p))
+                
+
+print("Candidate edges after pruning: ", e_clean)
 e_star=find_e(G, s, t, d, e_clean)
 print("Execution completed e*: (",e_star[0],", ",e_star[1], ")")
-print(e_star)
 
 
