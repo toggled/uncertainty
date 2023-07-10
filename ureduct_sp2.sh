e7479f636f6cf2501cde8f5136045c2ea017bcfa
Naheed <naheedcse@gmail.com>
2023-07-10 13:21:03 +0800

    xxy

--- ureduct_sp2.sh
+++ ureduct_sp2.sh
@@ -12,7 +12,7 @@ for k in {2..2}; do
    for K in "${NT_array[@]}"; do
       python reduce_main.py -k $k  -pr $pr  -a greedymem -K $K -ea $ea -d ER_15_22 -q data/queries/ER/ER_15_22_2.queries &
       # python reduce_main.py -k $k  -pr $pr  -a greedymem -K $K -ea exact -d ER_15_22 -q data/queries/ER/ER_15_22_2.queries &
-      python reduce_main.py -k $k  -pr $pr  -a greedymem -K $K -ea $ea -d ER_15_22 -q data/queries/ER/ER_15_22_4.queries &
+      # python reduce_main.py -k $k  -pr $pr  -a greedymem -K $K -ea $ea -d ER_15_22 -q data/queries/ER/ER_15_22_4.queries &
       # python reduce_main.py -k $k  -pr $pr  -a greedymem -K $K -ea exact -d ER_15_22 -q data/queries/ER/ER_15_22_4.queries &
       # python reduce_main.py -k $k -K $K -pr $pr -a greedy -ea mcdij -d ER_15_22 -q data/queries/ER/ER_15_22_2.queries &
       # python reduce_main.py -k $k -K $K -pr $pr -a greedy -ea exact -d ER_15_22 -q data/queries/ER/ER_15_22_2.queries &
@@ -20,8 +20,13 @@ for k in {2..2}; do
       # python reduce_main.py -k $k -K $K -pr $pr -a greedy -ea exact -d ER_15_22 -q data/queries/ER/ER_15_22_4.queries 
    done
    python reduce_main.py -k $k -pr $pr -a greedy -ea $ea -d ER_15_22 -q data/queries/ER/ER_15_22_2.queries &
-   python reduce_main.py -k $k -pr $pr -a greedy -ea $ea -d ER_15_22 -q data/queries/ER/ER_15_22_4.queries &
+   # python reduce_main.py -k $k -pr $pr -a greedy -ea $ea -d ER_15_22 -q data/queries/ER/ER_15_22_4.queries &
 done
+for K in "${NT_array[@]}"; do
+      # python reduce_main.py -k $k  -pr $pr  -a greedymem -K $K -ea exact -d ER_15_22 -q data/queries/ER/ER_15_22_2.queries &
+      python reduce_main.py -k 4  -pr $pr  -a greedymem -K $K -ea $ea -d ER_15_22 -q data/queries/ER/ER_15_22_4.queries &
+   done
+python reduce_main.py -k 4 -pr $pr -a greedy -ea $ea -d ER_15_22 -q data/queries/ER/ER_15_22_4.queries &
 # for k in {1..2}; do
 #    python reduce_main.py -k $k  -pr $pr -a exact -d ER_15_22 -q data/queries/ER/ER_15_22_2.queries &
 #    python reduce_main.py -k $k  -pr $pr -a exact -d ER_15_22 -q data/queries/ER/ER_15_22_4.queries &
