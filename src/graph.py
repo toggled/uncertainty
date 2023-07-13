6cbc2dddc53fb22a3828db86545bd0f526773ed0
Naheed <naheedcse@gmail.com>
2023-07-13 22:04:42 +0800

    script

--- src/graph.py
+++ src/graph.py
@@ -94,7 +94,7 @@ class UGraph:
     
     def find_rel_rss(self,T, source,target,seed = 1, optimiseargs = {'nbrs':None, 'doopt': False}):
         # print('source: ',source, ' target: ',target)
-        kRecursiveSamplingThreshold = 10
+        kRecursiveSamplingThreshold = 5
         states = ["0001","0010","0011","0100","0101",\
                   "0110","0111","1000","1001","1010",\
                  "1011","1100","1101","1110","1111",\
