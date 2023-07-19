aa6a2851b359e0805bc92f7521bb20602f613edf
Naheed <naheedcse@gmail.com>
2023-07-19 19:42:30 +0800

    greedyP

--- ureduct_reach2.sh
+++ ureduct_reach2.sh
@@ -19,15 +19,15 @@ for k in {2..2}; do
    #    # python reduce_main.py -k $k -K $K -pr reach -a greedy -ea mcbfs -d ER_15_22 -q data/queries/ER/ER_15_22_4.queries &
    #    # python reduce_main.py -k $k -K $K -pr reach -a greedy -ea exact -d ER_15_22 -q data/queries/ER/ER_15_22_4.queries 
    # done
-   python reduce_main.py -k $k -pr reach -a greedy -ea $ea -d ER_15_22 -q data/queries/ER/ER_15_22_2.queries &
+   python reduce_main.py -k $k -pr reach -a greedyp -ea $ea -d ER_15_22 -q data/queries/ER/ER_15_22_2.queries &
    # python reduce_main.py -k $k -pr reach -a greedy -ea $ea -d ER_15_22 -q data/queries/ER/ER_15_22_4.queries &
 done
 # for K in "${NT_array[@]}"; do
 #    # python reduce_main.py -k $k  -pr reach  -a greedymem -K $K -ea exact -d ER_15_22 -q data/queries/ER/ER_15_22_2.queries &
 #    python reduce_main.py -k 4  -pr reach  -a greedymem -K $K -ea $ea -d ER_15_22 -q data/queries/ER/ER_15_22_4.queries &
 # done
-python reduce_main.py -k 4 -pr reach -a greedy -ea $ea -d ER_15_22 -q data/queries/ER/ER_15_22_4.queries &
-python reduce_main.py -k 4  -pr reach -a exact -d ER_15_22 -q data/queries/ER/ER_15_22_4.queries &
+python reduce_main.py -k 4 -pr reach -a greedyp -ea $ea -d ER_15_22 -q data/queries/ER/ER_15_22_4.queries &
+# python reduce_main.py -k 4  -pr reach -a exact -d ER_15_22 -q data/queries/ER/ER_15_22_4.queries &
 # for k in {1..2}; do
 #    python reduce_main.py -k $k  -pr reach -a exact -d ER_15_22 -q data/queries/ER/ER_15_22_2.queries &
 #    python reduce_main.py -k $k  -pr reach -a exact -d ER_15_22 -q data/queries/ER/ER_15_22_4.queries &
--- ureduct_sp2.sh
+++ ureduct_sp2.sh
@@ -9,25 +9,25 @@ NT_array=($((NT/4)) $((NT/2)) $((NT)) $((NT*2)) $((NT*4)) $((NT*8)) $((NT*16)))
 for k in {2..2}; do
    # for i in {0..6}; do 
    #    K=$((2**i)); 
-   for K in "${NT_array[@]}"; do
-      python reduce_main.py -k $k  -pr $pr  -a greedymem -K $K -ea $ea -d ER_15_22 -q data/queries/ER/ER_15_22_2.queries &
-      # python reduce_main.py -k $k  -pr $pr  -a greedymem -K $K -ea exact -d ER_15_22 -q data/queries/ER/ER_15_22_2.queries &
-      # python reduce_main.py -k $k  -pr $pr  -a greedymem -K $K -ea $ea -d ER_15_22 -q data/queries/ER/ER_15_22_4.queries &
-      # python reduce_main.py -k $k  -pr $pr  -a greedymem -K $K -ea exact -d ER_15_22 -q data/queries/ER/ER_15_22_4.queries &
-      # python reduce_main.py -k $k -K $K -pr $pr -a greedy -ea mcdij -d ER_15_22 -q data/queries/ER/ER_15_22_2.queries &
-      # python reduce_main.py -k $k -K $K -pr $pr -a greedy -ea exact -d ER_15_22 -q data/queries/ER/ER_15_22_2.queries &
-      # python reduce_main.py -k $k -K $K -pr $pr -a greedy -ea mcdij -d ER_15_22 -q data/queries/ER/ER_15_22_4.queries &
-      # python reduce_main.py -k $k -K $K -pr $pr -a greedy -ea exact -d ER_15_22 -q data/queries/ER/ER_15_22_4.queries 
-   done
-   python reduce_main.py -k $k -pr $pr -a greedy -ea $ea -d ER_15_22 -q data/queries/ER/ER_15_22_2.queries &
+   # for K in "${NT_array[@]}"; do
+   #    python reduce_main.py -k $k  -pr $pr  -a greedymem -K $K -ea $ea -d ER_15_22 -q data/queries/ER/ER_15_22_2.queries &
+   #    # python reduce_main.py -k $k  -pr $pr  -a greedymem -K $K -ea exact -d ER_15_22 -q data/queries/ER/ER_15_22_2.queries &
+   #    # python reduce_main.py -k $k  -pr $pr  -a greedymem -K $K -ea $ea -d ER_15_22 -q data/queries/ER/ER_15_22_4.queries &
+   #    # python reduce_main.py -k $k  -pr $pr  -a greedymem -K $K -ea exact -d ER_15_22 -q data/queries/ER/ER_15_22_4.queries &
+   #    # python reduce_main.py -k $k -K $K -pr $pr -a greedy -ea mcdij -d ER_15_22 -q data/queries/ER/ER_15_22_2.queries &
+   #    # python reduce_main.py -k $k -K $K -pr $pr -a greedy -ea exact -d ER_15_22 -q data/queries/ER/ER_15_22_2.queries &
+   #    # python reduce_main.py -k $k -K $K -pr $pr -a greedy -ea mcdij -d ER_15_22 -q data/queries/ER/ER_15_22_4.queries &
+   #    # python reduce_main.py -k $k -K $K -pr $pr -a greedy -ea exact -d ER_15_22 -q data/queries/ER/ER_15_22_4.queries 
+   # done
+   python reduce_main.py -k $k -pr $pr -a greedyp -ea $ea -d ER_15_22 -q data/queries/ER/ER_15_22_2.queries &
    # python reduce_main.py -k $k -pr $pr -a greedy -ea $ea -d ER_15_22 -q data/queries/ER/ER_15_22_4.queries &
 done
-for K in "${NT_array[@]}"; do
-   # python reduce_main.py -k $k  -pr $pr  -a greedymem -K $K -ea exact -d ER_15_22 -q data/queries/ER/ER_15_22_2.queries &
-   python reduce_main.py -k 4  -pr $pr  -a greedymem -K $K -ea $ea -d ER_15_22 -q data/queries/ER/ER_15_22_4.queries &
-done
-python reduce_main.py -k 4 -pr $pr -a greedy -ea $ea -d ER_15_22 -q data/queries/ER/ER_15_22_4.queries &
-python reduce_main.py -k 4 -pr $pr -a exact -d ER_15_22 -q data/queries/ER/ER_15_22_4.queries &
+# for K in "${NT_array[@]}"; do
+#    # python reduce_main.py -k $k  -pr $pr  -a greedymem -K $K -ea exact -d ER_15_22 -q data/queries/ER/ER_15_22_2.queries &
+#    python reduce_main.py -k 4  -pr $pr  -a greedymem -K $K -ea $ea -d ER_15_22 -q data/queries/ER/ER_15_22_4.queries &
+# done
+python reduce_main.py -k 4 -pr $pr -a greedyp -ea $ea -d ER_15_22 -q data/queries/ER/ER_15_22_4.queries &
+# python reduce_main.py -k 4 -pr $pr -a exact -d ER_15_22 -q data/queries/ER/ER_15_22_4.queries &
 # for k in {1..2}; do
 #    python reduce_main.py -k $k  -pr $pr -a exact -d ER_15_22 -q data/queries/ER/ER_15_22_2.queries &
 #    python reduce_main.py -k $k  -pr $pr -a exact -d ER_15_22 -q data/queries/ER/ER_15_22_4.queries &
